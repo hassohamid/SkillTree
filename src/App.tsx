6af83878288cf1064609b67b85f418ca4c9f978e
hassohamid <hassohamid@hotmail.com>
2025-06-22 12:04:26 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl tracking-tighter text-muted-foreground">
+          SkillTree
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
